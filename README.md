08e1c25d64e61ec05df887d85673ccb3ebb376d9
unknown <dinaam.n@gmail.com>
2020-06-13 15:34:07 +0200

    First major functionality

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# :construction: Discord bot :construction:<br>
-Simple projet to discover Discord.js <br>
+# :robot: Discord bot :robot:<br>
+Simple project to discover Discord.js <br>
+
+## First Functionality 
+Message loop (if !help command) till it's handled by someone (CheckMark reaction)
+
+## Project setup :wrench:
+
+*FIRST OF ALL : Download or clone the project locally*<br>
+*Duplicate .env.dist file in .env and change the value of TOKEN and the value of by yours*
+
+<br>
+
+
+## Discord.js preparations [https://discordjs.guide/preparations/]
--- bot.js
+++ bot.js
@@ -1,59 +1,55 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-
-client.on('message', message => {
-    if (message.content.includes('!help')) {
-        loopTillHandled(message);
-    }
-
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
 
-client.on("messageReactionAdd", async (reaction, user) => {
-  // When we receive a reaction we check if the reaction is partial or not
-  if (reaction.partial) {
-    // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-    try {
-      await reaction.fetch();
-    } catch (error) {
-      console.log("Something went wrong when fetching the message: ", error);
-      return;
-    }
-  }
-  console.log(`${ reaction.message }`);
-  if (reaction.emoji.name == "👌") {
-
+client.on("message", (message) => {
+  if (message.content.includes("!help")) {
+    loopTillHandled(message);
   }
 });
 
 function loopTillHandled(message) {
-  
-  setInterval(displayEmbed, 1000, message);
+  const boucle = setInterval(
+    (message) => {
+      message.channel.messages
+        .fetch(message.id)
+        .then((msg) => {
+          const item = msg.reactions.cache.find((elem) => {
+            return elem.emoji.name == "✅";
+          });
+          if (item !== undefined) {
+            clearInterval(boucle);
+            return;
+          }
+          message.channel.send({
+            embed: {
+              color: 15158332,
+              author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL,
+              },
+              fields: [
+                {
+                  name: "Bah alors ?! Personne veut traiter ce message : ",
+                  value: message.content,
+                },
+              ],
+              timestamp: new Date(),
+              footer: {
+                icon_url: client.user.avatarURL,
+                text: "© ITEA",
+              },
+            },
+          });
+        })
+        .catch(console.error);
+        
+    },
+    15000,
+    message
+  );
 }
 
-function displayEmbed(message) {
-  console.log("message", message);
-  const embed = new Discord.MessageEmbed();
-  message.channel.send({
-    embed: {
-      color: 15158332,
-      author: {
-        name: client.user.username,
-        icon_url: client.user.avatarURL,
-      },
-      fields: [
-        {
-          name: "Bah alors ?! Personne veut traiter ce message : ",
-          value: message.content,
-        },
-      ],
-      timestamp: new Date(),
-      footer: {
-        icon_url: client.user.avatarURL,
-        text: "© ITEA",
-      },
-    },
-  });
-  console.log("ok => ", message.content);
-}
 client.login(process.env.TOKEN);
